93a6610cdd918be33fe3fa97be25237839f84b0a
MostafaSewidan <sewidanmostafa@gmail.com>
2022-06-05 17:25:01 +0200

    translate cities

--- src/Models/City.php
+++ src/Models/City.php
@@ -25,4 +25,42 @@ class City extends Model
 	{
 		return config('world.migrations.cities.table_name', parent::getTable());
 	}
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        /**
+         * Write code on Method
+         *
+         * @return response()
+         */
+        static::created(function ($item) {
+
+            $state_ar = include __DIR__.'/../../resources/lang/ar/cities.php';
+
+            if(array_key_exists($item->id , $state_ar)){
+
+                $title = [
+                    'ar' => $state_ar[$item->id],
+                    'en' => $item->name
+                ];
+            }else{
+
+                $title = [
+                    'ar' => $item->name,
+                    'en' => $item->name
+                ];
+            }
+
+            $item->name = $title;
+            $item->title = $title;
+            $item->save();
+        });
+    }
 }
